ee755f06a99d2a885c95ca462ee2873dbb44cab1
บัณฑิต คุ้มสวัสดิ์ <85699339+bandiddeaware@users.noreply.github.com>
2022-02-06 00:14:43 +0700

    docker-compose deploy backend api.

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 11110, function(){
+var server = app.listen( process.env.BACKEND_PORT, function(){
   console.log('Listening on port ' + server.address().port);
 });
--- src/controller/connection.js
+++ src/controller/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql2/promise');
 
 const connection = () => {
   var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST_DEV,
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_ROOT,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
